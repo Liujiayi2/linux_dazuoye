8be2e91d05e4234337be31406407b2ebb9728383
cf_2844545327 <2844545327@qq.com>
2022-12-31 22:37:09 +0800

    新建一个服务端

---
+++ SocketService.java
@@ -0,0 +1,31 @@
+public class SocketService{
+	public static void main(String[] args) throws IOException{
+		SocketService socketService = new SocketService();
+		socketService.oneServer();}
+	public void oneServer(){
+		try{
+			ServerSocket server = null;
+			try{
+				server = new ServerSocket(2022);
+				System.out.println("Service enable Success");}
+			catch(Exception e){System.out.println("No Listen:" + e);}
+			Socket socket = null;
+			try{socket = server.accept();}
+			catch(Exception e){System.out.println("Error." + e);}
+			String line;
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Client:" + in.readLine());
+			line = br.readLine();
+			while(!line.equals("end")){
+				writer.println(line);
+				writer.flush();
+				System.out.println("Service:" + line);
+				System.out.println("Client:" + in.readLine());
+				line = br.readLine();}
+			writer.close();
+			in.close();
+			socket.close();
+			server.close();}
+		catch(Exception e){System.out.println("Error." + e);}}}
